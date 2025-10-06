990f61a1a6529851c9aed37c6dae7ac1bf7c839d
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-10-06 20:56:45 -0300

    BeforeEach

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,11 +1,20 @@
 import org.example.Calculadora;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+    private Calculadora calculadora;
+
+    @BeforeEach
+    public void setUp(){
+        calculadora = new Calculadora();
+        System.out.println("..");
+    }
+
     @Test
     public void deveSomarDoisValores(){
-        Calculadora calculadora = new Calculadora();
         double a = 10;
         double b = 20;
 
@@ -15,7 +24,6 @@ public class CalculadoraTest {
     }
     @Test
     public void deveSomarMultiplicarValores(){
-        Calculadora calculadora = new Calculadora();
         double a = 10;
         double b = 20;
 
@@ -25,7 +33,6 @@ public class CalculadoraTest {
     }
     @Test
     public void deveSubtrairDoisValores(){
-        Calculadora calculadora = new Calculadora();
         double a = 10;
         double b = 20;
 
@@ -35,12 +42,16 @@ public class CalculadoraTest {
     }
     @Test
     public void deveDividirDoisValores(){
-        Calculadora calculadora = new Calculadora();
         double a = 10;
         double b = 20;
 
         double resultado = calculadora.dividir(a, b);
 
         Assertions.assertEquals(0.5, resultado);
+    }
+
+    @AfterEach
+    public void depoisDeCada(){
+        System.out.println(".,");
     }
 }
